a465a0fdfae7c047e8127fb429df7111093d1d1e
MasumRezaMunna <masumrezamunna2005@gmail.com>
2026-01-11 23:22:17 +0600

    Navigate Component

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import { Navigate } from 'react-router';
 
 const Home = () => {
     return (
-        <div>
-            <p>home page is showing</p>
-        </div>
+        <Navigate to="/category/1"></Navigate>
     );
 };
 
