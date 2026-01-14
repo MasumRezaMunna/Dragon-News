b56670f116f1e460698b59b245ba66c3ea2ded94
MasumRezaMunna <masumrezamunna2005@gmail.com>
2026-01-14 09:18:54 +0600

    logOut & Login Functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,15 @@ import userIcon from "../assets/user.png"
 import { AuthContext } from "../provider/AuthProvider";
 
 const Navbar = () => {
-  const {user} = use(AuthContext);
+  const {user, logOut} = use(AuthContext);
+  const handleLogOut = () =>{
+    console.log("user trying to logout");
+    logOut().then(() =>{
+        alert("You logged Out Successfully")
+    }).catch((error) =>{
+        console.log(error)
+    })
+  }
   return <div className="flex justify-between items-center">
     <div className="">{user && user.email}</div>
     <div className="nav flex gap-5 text-accent">
@@ -14,7 +22,14 @@ const Navbar = () => {
     </div>
     <div className="login-btn flex gap-5">
       <img src={userIcon} alt="" />
-      <Link to="/auth/login" className="btn btn-primary px-10">Login</Link>
+      {
+        user ? ( <button onClick={handleLogOut} className="btn btn-primary px-10">LogOut</button>
+
+        ) : (
+        
+        <Link to="/auth/login" className="btn btn-primary px-10">Login</Link>)
+      }
+      
     </div>
   </div>;
 };
